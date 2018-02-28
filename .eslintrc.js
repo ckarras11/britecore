fb34b1068bbc0a3b4062b90c943f79c131fb3be3
Christopher Karras <ckarras11@gmail.com>
2018-02-28 09:59:14 -0500

    Added ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    "extends": "airbnb",
+    "rules": {
+        "react/jsx-filename-extension": 0,
+        "react/prop-types": 0,
+        "jsx-a11y/no-noninteractive-element-interactions": 0,
+        "jsx-a11y/click-events-have-key-events": 0,
+        "jsx-a11y/label-has-for": 0,
+        "react/no-array-index-key": 0,
+    }
+};
