33f1a0111f850116ba81def0053fdc0a7d2abd7b
Christopher Karras <ckarras11@gmail.com>
2018-02-28 12:09:22 -0500

    Added a list of tags

---
+++ src/components/tag-list.js
@@ -0,0 +1,13 @@
+const tags = [
+    {
+        name: 'Vinmaster',
+        subtag: ['make', 'model', 'year'],
+    },{
+        name: 'ISO',
+        subtag: ['ISO1', 'ISO2', 'ISO3'],
+    },{
+        name: 'Tag3',
+        subtag: ['subtag1', 'subtag2', 'subtag3'],
+    },
+]
+export default tags;
