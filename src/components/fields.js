0c3e3288b4561d07d85497587c5a27b51baf6fb0
Christopher Karras <ckarras11@gmail.com>
2018-02-28 17:08:50 -0500

    Connected fields and search to redux

--- src/components/fields.js
+++ src/components/fields.js
@@ -1,27 +1,35 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import SingleField from './field';
 import fieldTypes from './field-types';
+import { setFilter, setFieldType } from '../actions';
 
+function mapStateToProps(state) {
+    return {
+        filter: state.filter,
+    };
+  }
 
 class Fields extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {
-            filter: '',
-        };
+
         this.handleFilter = this.handleFilter.bind(this);
+        this.fieldSelect = this.fieldSelect.bind(this);
     }
     handleFilter(e) {
-        this.setState({
-            filter: e.currentTarget.value,
-        });
+        const query = e.currentTarget.value.toLowerCase();
+        this.props.dispatch(setFilter(query));
+    }
+    fieldSelect(e) {
+        this.props.dispatch(setFieldType(e.currentTarget.id));
     }
     render() {
-        const filteredItems = fieldTypes.filter(item => item.type.includes(this.state.filter));
+        const filteredItems = fieldTypes.filter(item => item.type.includes(this.props.filter));
         const fieldItems = filteredItems.map((field, index) => {
             const firstLetter = field.type.charAt(0).toUpperCase();
             const title = `${firstLetter}${field.type.slice(1)}`;
-            return <li onClick={this.props.onClick} id={field.type} key={index}><SingleField type={title} definition={field.definition} /></li>;
+            return <li onClick={this.fieldSelect} id={field.type} key={index}><SingleField type={title} definition={field.definition} /></li>;
         });
         return (
           <div className="field-container">
@@ -36,4 +44,4 @@ class Fields extends React.Component {
     }
 }
 
-export default Fields;
+export default connect(mapStateToProps)(Fields);
