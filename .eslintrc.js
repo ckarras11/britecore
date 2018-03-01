9798d302b9a8d699e35a554cccf46a9de6a946cc
Christopher Karras <ckarras11@gmail.com>
2018-03-01 14:48:57 -0500

    Linting rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,4 @@
-module.exports = {
+/* module.exports = {
     "extends": "airbnb",
     "rules": {
         "react/jsx-filename-extension": 0,
@@ -7,5 +7,10 @@ module.exports = {
         "jsx-a11y/click-events-have-key-events": 0,
         "jsx-a11y/label-has-for": 0,
         "react/no-array-index-key": 0,
+        "no-plusplus": 0,
+        "no-return-assign": 0,
+        "no-param-reassign": 0,
+        "import/first": 0,
+
     }
-};
+}; */
